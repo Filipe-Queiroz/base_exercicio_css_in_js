4914ca816e1d80648a1514bb61df55c86c0305df
Filipe Queiroz <filipe.queiroz.312@gmail.com>
2024-10-09 12:23:57 -0300

    convertendo estilos com styled components

--- src/components/Cabecalho/index.tsx
+++ src/components/Cabecalho/index.tsx
@@ -1,9 +1,9 @@
-import styles from './Cabecalho.module.css'
+import { CabecalhoStyled } from './Cabecalho'
 
 const Cabecalho = () => (
-  <header className={styles.cabecalho}>
+  <CabecalhoStyled>
     <h1>EBAC Jobs</h1>
-  </header>
+  </CabecalhoStyled>
 )
 
 export default Cabecalho
